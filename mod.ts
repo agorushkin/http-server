410bd1fa75765290f5751af279079ba3359ec71d
Arsenii Gorushkin <arsenii.gorushkin@gmail.com>
2023-05-30 21:30:44 +1200

    feat: removed utils
    Utility functions will later be available under the new, separate repo.

--- mod.ts
+++ mod.ts
@@ -2,5 +2,3 @@ export { HttpServer } from './lib/server.ts';
 
 export type { Handler } from './lib/server.ts';
 export type { HttpRequest, HttpResponse } from './lib/event.ts';
-
-export { file, files } from './lib/utils.ts';
