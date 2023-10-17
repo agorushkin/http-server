4e32b3186262504f23663ca547407b64f6dcdbb8
Arsenii Gorushkin <arsenii.gorushkin@gmail.com>
2023-10-17 20:30:05 +1300

    fix: fixed full stop in `extension()`

--- lib/utils/extension.ts
+++ lib/utils/extension.ts
@@ -2,7 +2,7 @@ import { MIME_TYPES } from './mime-types.ts';
 
 export const extension = (path: string) => {
   const extension = path.split('.').pop();
-  const mime = MIME_TYPES[ `.${ extension }` ] ?? MIME_TYPES['.txt'];
+  const mime = MIME_TYPES[ `${ extension }` ] ?? MIME_TYPES[ 'txt' ];
 
   return mime;
 };
